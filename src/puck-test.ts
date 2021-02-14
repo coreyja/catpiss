8dd85942f123f4a563bee77dd19d91249b1f87dc
Corey Alexander <coreyja@gmail.com>
2021-02-13 19:49:21 -0500

    Add eslint and prettier, editor integration is enough for now no scripts

--- src/puck-test.ts
+++ src/puck-test.ts
@@ -1,30 +1,32 @@
-declare const LED1: Pin
-declare const LED2: Pin
-declare const LED3: Pin
+declare const LED1: Pin;
+declare const LED2: Pin;
+declare const LED3: Pin;
 
-declare const BTN: Pin
+declare const BTN: Pin;
 
 const LEDS = [LED1, LED2, LED3];
 
 const blinkAll = () => {
-  LEDS.forEach(led => led.set())
-  LEDS.forEach(led => led.reset())
-}
+  LEDS.forEach((led) => led.set());
+  LEDS.forEach((led) => led.reset());
+};
 
 // Basically a wrapper to hide my `any` cast as much as possible
 // This method isn't in the type definitions
 const setNfcUrl = (url: string) => {
-  (NRF as any).nfcURL(url);
-}
+  (NRF as any).nfcURL(url); // eslint-disable-line @typescript-eslint/no-explicit-any
+};
 
 const onButton = () => {
   const whenPressed = Date.now();
   LED1.set();
   setNfcUrl(`https://coreyja.com?now=${whenPressed.toString()}`);
 
-  setTimeout(() => { LED1.reset() }, 5000)
-}
+  setTimeout(() => {
+    LED1.reset();
+  }, 5000);
+};
 
-setWatch(onButton, BTN, { edge: 'rising', debounce: 50, repeat: true })
+setWatch(onButton, BTN, { edge: "rising", debounce: 50, repeat: true });
 
-blinkAll()
+blinkAll();
