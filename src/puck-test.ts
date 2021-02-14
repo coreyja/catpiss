4e69602a1550f118eaa4cea8e0f36c91def20a6a
Corey Alexander <coreyja@gmail.com>
2021-02-13 19:58:04 -0500

    Add ability to do stuff on a long press

--- src/puck-test.ts
+++ src/puck-test.ts
@@ -1,3 +1,4 @@
+//// GLOBALS
 declare const LED1: Pin;
 declare const LED2: Pin;
 declare const LED3: Pin;
@@ -5,6 +6,8 @@ declare const LED3: Pin;
 declare const BTN: Pin;
 
 const LEDS = [LED1, LED2, LED3];
+
+//// UTILITIES
 
 const blinkAll = () => {
   LEDS.forEach((led) => led.set());
@@ -17,16 +20,44 @@ const setNfcUrl = (url: string) => {
   (NRF as any).nfcURL(url); // eslint-disable-line @typescript-eslint/no-explicit-any
 };
 
-const onButton = () => {
-  const whenPressed = Date.now();
-  LED1.set();
-  setNfcUrl(`https://coreyja.com?now=${whenPressed.toString()}`);
+const onLongPress = (callback: () => void, longPressDuration = 2000) => {
+  setWatch(
+    () => {
+      const longPressTimeoutId = setTimeout(() => {
+        callback();
+      }, longPressDuration);
 
-  setTimeout(() => {
-    LED1.reset();
-  }, 5000);
+      setWatch(
+        () => {
+          clearTimeout(longPressTimeoutId);
+        },
+        BTN,
+        { edge: "falling", repeat: false }
+      );
+    },
+    BTN,
+    { edge: "rising", repeat: true }
+  );
 };
 
-setWatch(onButton, BTN, { edge: "rising", debounce: 50, repeat: true });
+//// BUSINESS LOGIC
+
+// const onButton = () => {
+//   const whenPressed = Date.now();
+//   LED1.set();
+//   setNfcUrl(`https://coreyja.com?now=${whenPressed.toString()}`);
+
+//   setTimeout(() => { LED1.reset() }, 5000)
+// }
+
+// setWatch(onButton, BTN, { edge: 'rising', debounce: 50, repeat: true })
+
+onLongPress(() => {
+  console.log("Long Press Achieved");
+  LED3.set();
+  setTimeout(() => {
+    LED3.reset();
+  }, 300);
+}, 2000);
 
 blinkAll();
