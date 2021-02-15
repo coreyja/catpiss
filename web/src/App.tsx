c994f783c4abf03cb0754f53b9462b8cdd618e07
Corey Alexander <coreyja@gmail.com>
2021-02-15 16:23:35 -0500

    Get a nice battery indicator done

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -28,11 +28,11 @@ const timeAgoInWords = (timeInPast: Date): string => {
 
 const backgroundColorClassForBatteryLevel = (level: number) => {
   if (level > 50) {
-    return 'bg-green-500';
+    return 'text-green-500';
   } else if (level > 25) {
-    return 'bg-yellow-500';
+    return 'text-yellow-500';
   } else {
-    return 'bg-red-500';
+    return 'text-red-500';
   }
 }
 
@@ -41,27 +41,17 @@ const Battery: FunctionComponent<BatteryProps> = ({batteryInfo}) => {
 
   const batteryColorClass = backgroundColorClassForBatteryLevel(batteryInfo.batteryLevel)
 
-  const batteryCachedAtAgoMillis = new Date().valueOf() - batteryInfo.batteryCachedAt.valueOf()
-  const batteryCachedAtAgoHours = batteryCachedAtAgoMillis / 1000 / 60 / 60;
-
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="relative flex w-24 my-1 border-2 border-gray-400 rounded shadow">
-        <div
-          className="absolute z-10 flex h-6 mt-2 ml-24 border-r-8 border-gray-400 rounded-r"></div>
-        <div
-          className={`flex items-center justify-center py-4 m-1 text-xs font-bold leading-none text-center text-white ${batteryColorClass} cursor-default`}
-          style={{width: `${batteryInfo.batteryLevel}%`}}>
-          <div className="absolute left-0 w-full text-white">
-            {`${batteryInfo.batteryLevel}%`}
-          </div>
-        </div>
-      </div>
-
-      <div>
-        As of {timeAgoInWords(batteryInfo.batteryCachedAt)}
-      </div>
-    </div>
+  <figure className="flex flex-row items-center justify-center my-2">
+    <svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 640 512" xmlns="http://www.w3.org/2000/svg" className="w-8 mx-2">
+      <path fill="currentColor" d="M 544 160 L 544 224 L 576 224 L 576 288 L 544 288 L 544 352 L 64 352 L 64 160 L 544 160 M 560 96 L 48 96 C 21.49 96 0 117.49 0 144 L 0 368 C 0 394.51 21.49 416 48 416 L 560 416 C 586.51 416 608 394.51 608 368 L 608 352 L 616 352 C 629.255 352 640 341.255 640 328 L 640 184 C 640 170.745 629.255 160 616 160 L 608 160 L 608 144 C 608 117.49 586.51 96 560 96 Z"></path>
+      <rect x="71.242" y="166.969" width={`${465 * batteryInfo.batteryLevel / 100.0}`} className={`${batteryColorClass} fill-current`} height="178.936"></rect>
+    </svg>
+    <figcaption className="mx-2 text-xs">
+      <p>Battery Percent: {batteryInfo.batteryLevel}%</p>
+      <p>As of {timeAgoInWords(batteryInfo.batteryCachedAt)}</p>
+    </figcaption>
+  </figure>
   )
 }
 
@@ -81,8 +71,8 @@ function App() {
 
   // Return the App component.
   return (
-    <div className="min-h-screen text-center text-white bg-gray-800">
-      <header className="flex flex-col items-center justify-center py-8">
+    <div className="max-w-md min-h-screen m-auto text-center text-white">
+      <header className="flex flex-col items-center justify-center pt-8 pb-2">
         <h1 className="mb-8 text-4xl text-white">Catpiss</h1>
         <figure>
           <img src={blackCat} className="mb-2 h-72" alt="Black cat in flower pot" />
